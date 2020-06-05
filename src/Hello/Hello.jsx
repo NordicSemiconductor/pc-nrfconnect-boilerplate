a5ca8448474e21517a62e20eebb6d4ea92e74d04
Marko Schulz <Marko.Schulz@nordicsemi.no>
2020-06-05 11:59:54 +0200

    Add example for component styling

--- src/Hello/Hello.jsx
+++ src/Hello/Hello.jsx
@@ -37,10 +37,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import './hello.scss';
+
 const Hello = props => {
     const { title } = props;
     return (
-        <h3>{ title }</h3>
+        <h3 className="title">{ title }</h3>
     );
 };
 
