d74654b462a0185d2c8c46e98bb550d06c5c2e78
Marko Schulz <Marko.Schulz@nordicsemi.no>
2020-06-08 18:30:20 +0200

    Demonstrate the use of panes better
    By adding a second example pane.

--- src/Hello/Goodbye.jsx
+++ src/Hello/Goodbye.jsx
@@ -35,35 +35,9 @@
  */
 
 import React from 'react';
-import { App } from 'pc-nrfconnect-shared';
 
-import SidePanel from './SidePanel';
-import BoilerplateDeviceSelector from './BoilerplateDeviceSelector';
-import Hello from './Hello/Hello';
-import Goodbye from './Hello/Goodbye';
-
-import './index.scss';
-
-// If your app uses redux, then specify your root reducer here.
-// It will handle the slice of state under the name `app`.
-const reducer = undefined;
-
-// nRF Connect boilerplate app
-// ===========================
-//
-// In this boilerplate app, we show a dummy implementation of an nRF Connect
-// for Desktop app. We try to show every function but either only provide the
-// simplest implementation we could think of or just have a commented out
-// implementation to hint at what might be used. Adapt everything to make it
-// fit for you and remove what is not needed.
 export default () => (
-    <App
-        appReducer={reducer}
-        deviceSelect={<BoilerplateDeviceSelector />}
-        sidePanel={<SidePanel />}
-        panes={[
-            ['Hello', Hello],
-            ['Goodbye', Goodbye],
-        ]}
-    />
+    <h3>
+        So long, and thanks for all the fish
+    </h3>
 );
